6b9ee10c050caff96d6058ddd14bcec985a106f6
Brian May <brian@linuxpenguins.xyz>
2021-12-03 17:29:56 +1100

    tesla: Extract payload from MQTT messages

--- lib/penguin_nodes/flows/tesla.ex
+++ lib/penguin_nodes/flows/tesla.ex
@@ -10,6 +10,9 @@ defmodule PenguinNodes.Flows.Tesla do
   alias PenguinNodes.Nodes.Nodes
   alias PenguinNodes.Nodes.Simple
 
+  @spec payload_func(Mqtt.Message.t()) :: any()
+  defp payload_func(%Mqtt.Message{payload: payload}), do: payload
+
   @spec geofence_to_message(Simple.Changed.Message.t()) :: String.t()
   defp geofence_to_message(%Simple.Changed.Message{old: nil, new: new}),
     do: "The Tesla has arrived at #{new}"
@@ -39,6 +42,7 @@ defmodule PenguinNodes.Flows.Tesla do
       %{topic: ["teslamate", "cars", Integer.to_string(tesla), "geofence"]},
       id(:geofence_mqtt)
     )
+    |> call_value_value(Simple.Map, %{func: &payload_func/1}, id(:geofence_payload))
     |> call_value_value(Simple.Changed, %{}, id(:geofence_changed))
     |> call_value_value(Simple.Map, %{func: &geofence_to_message/1}, id(:geofence_to_message))
     |> message(id(:geofence_message))
@@ -49,6 +53,7 @@ defmodule PenguinNodes.Flows.Tesla do
       %{topic: ["teslamate", "cars", Integer.to_string(tesla), "plugged_in"], format: :json},
       id(:plugged_in_mqtt)
     )
+    |> call_value_value(Simple.Map, %{func: &payload_func/1}, id(:plugged_in_payload))
     |> call_value_value(Simple.Changed, %{}, id(:plugged_in_changed))
     |> call_value_value(Simple.Map, %{func: &plugged_in_to_message/1}, id(:plugged_in_to_message))
     |> message(id(:plugged_in_message))
