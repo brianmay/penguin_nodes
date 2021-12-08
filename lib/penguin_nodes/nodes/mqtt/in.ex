46e15af2bc20ed310095312389ca5a9a876bee39
Brian May <brian@linuxpenguins.xyz>
2021-12-08 12:36:20 +1100

    Make sure we subscribe to MQTT topic after restart

--- lib/penguin_nodes/nodes/mqtt/in.ex
+++ lib/penguin_nodes/nodes/mqtt/in.ex
@@ -41,6 +41,13 @@ defmodule PenguinNodes.Nodes.Mqtt.In do
   end
 
   @impl true
+  def restart(%NodeModule.State{} = state, %Node{} = node) do
+    %Options{} = options = node.opts
+    MqttMultiplexer.subscribe(options.topic, :mqtt, self(), options.format, options.resend)
+    {:ok, state}
+  end
+
+  @impl true
   def handle_cast({:mqtt, topic, :mqtt, payload}, %NodeModule.State{} = state) do
     message = %Message{
       topic: topic,
